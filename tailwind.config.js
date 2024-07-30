903faa38160063c810f9dc2e275365519069efe7
Ihasway <97021066+ihaswayorg@users.noreply.github.com>
2024-07-30 20:29:21 +0600

    Update tailwind.config.js
    Closes #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
+
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
